e313a2b98cc2a8454e0b5f2f8163c08a5ad1d5e9
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-17 15:40:45 -0300

    add component sobre

--- portifolio-reactjs/src/App.js
+++ portifolio-reactjs/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Sobre from "./components/Sobre";
 
 
 
 function App() {
-  
+
   return (
     <div>
       <Header/>
       <Main/>
+      <Sobre/>
     </div>
   );
 }
---
+++ portifolio-reactjs/src/components/Sobre/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Sobre = () => {
+  return (
+    <section>
+      <h1>Sobre</h1>
+      <div>
+        <p>
+          Para você que não me conhece, vou descrever algumas das minhas
+          principais características. A comunicação e ser uma pessoa de fácil
+          relacionamento se destacam das demais. Amo tecnologia, pois vejo como
+          esta facilita a vida cotidiana das pessoas, principalmente pelo fato
+          de possibilitar que todos tenham acesso a informação, justamente por
+          isso estou estudando e me empenhando em front-end (html, css, js,
+          react, Angular, dentre outras tecnologias).
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Sobre;
